cda4040656db06229e3712e53dbfc576c8463930
aarusharyal <aryalaarush203@gmail.com>
2026-06-26 21:42:06 +0545

    Added the necessary Comments for Better Understanding

--- add.js
+++ add.js
@@ -1,6 +1,8 @@
 let lastResult = null;
 
-function add(num1, num2) {
+function add(num1, num2)
+// Function to add two numbers and store the result.
+{
     lastResult = num1 + num2;
     return lastResult;
 }
